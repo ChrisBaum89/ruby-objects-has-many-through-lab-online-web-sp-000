6a76ec8ad6626a353b43305999b959a85719be37
Chris Baum <christopher.baum@valpo.edu>
2021-02-05 01:39:20 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -8,31 +8,11 @@ class Artist
   def initialize(name)
     @name = name
     @@all << self
-    binding.pry
+    #binding.pry
   end
 
   def self.all
     @@all
-  end
-
-  def songs
-    @songs = Song.all.select do |x|
-      x.artist == self
-    end
-    binding.pry
-  end
-
-  def genres
-    songs.map do |x|
-      x.genre
-    end
-  end
-
-  def new_song(name, genre)
-    genrename = genre.name
-    new_song = Song.new(name, self.name, genrename)
-    relaysongs = self.songs
-    #binding.pry
   end
 
 
