c99009b4bd4a7b8ab74d72d9897167ae4e4f6fa5
Chris Baum <christopher.baum@valpo.edu>
2021-02-05 01:26:36 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,7 +2,7 @@ class Song
   attr_accessor :name, :artist, :genre
   @@all = []
 
-  def initialize(name, artist, genre.name)
+  def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
@@ -12,6 +12,4 @@ class Song
   def self.all
     @@all
   end
-
-
 end
